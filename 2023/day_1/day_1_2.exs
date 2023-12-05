c60496830a21e75101027539d5767aa4150f5f2f
Matthew Rogers <alula_irl@proton.me>
2023-12-04 22:47:57 -0600

    update solutions

--- 2023/day_1/day_1_2.exs
+++ 2023/day_1/day_1_2.exs
@@ -5,16 +5,6 @@ abc_nums = %{"one" => "1", "two" => "2", "three" => "3",
   "four" => "4", "five" => "5", "six" => "6",
   "seven" => "7", "eight" => "8", "nine" => "9"}
 
-
-# Used to arrange queues for processing numbers : outside->in
-interleave_maps = fn {map1, map2} ->
-  [map1, map2]
-end
-
-alternate_lists = fn (list1, list2) ->
-  Enum.concat(Enum.zip(list1, list2) |> Enum.flat_map(&interleave_maps.(&1)))
-end
-
 sum = input
   |> String.split("\n")
   |> Enum.map(fn line ->
@@ -31,15 +21,13 @@ sum = input
       abc_nums_i = Enum.reject(abc_nums_i, fn abc_nums_i_m ->
         Map.values(abc_nums_i_m) |> hd == nil
       end)
+      # Used to arrange queues for processing numbers : outside->in
       abc_nums_i_sorted = abc_nums_i |> Enum.sort_by(fn map-> map |> Map.values() |> hd end)
-      abc_nums_i_r_sorted = abc_nums_i_sorted |> Enum.reverse
-      abc_nums_i = alternate_lists.(abc_nums_i_sorted, abc_nums_i_r_sorted)
-      abc_nums_i = Enum.take(abc_nums_i, div(length(abc_nums_i), 2))
-      IO.inspect(abc_nums_i)
+      abc_nums_i = if length(abc_nums_i) == 0, do: [], else: [Enum.at(abc_nums_i_sorted, 0), Enum.at(abc_nums_i_sorted, -1)] |> Enum.reverse
 
 
       n_line = Enum.reduce(abc_nums_i, line, fn abc_num, acc ->
-        {abc_num_k, _i} = abc_num
+        {abc_num_k, _i} = abc_num |> Map.to_list() |> hd
         if String.contains?(acc, abc_num_k) do
           String.replace(acc, abc_num_k, abc_nums[abc_num_k])
         else
